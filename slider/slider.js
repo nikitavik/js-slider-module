7cb6b2b3f7742cb8ef6127701d6f2323e104ab50
Nikita Kornilov <nikitakornilov1998@gmail.com>
2021-04-24 17:46:23 +0300

    Change position from left

--- slider/slider.js
+++ slider/slider.js
@@ -1,6 +1,7 @@
 const getTemplate = () => {
     return `
     <div class="slider-bar" data-type="bar" data->
+        <div class="slider-header" data-type="header"></div>
         <span class="slider-handle" data-type="handle"></span>
     </div>
     `
@@ -24,6 +25,7 @@ export class Slider {
     #setup() {
         this.$bar = this.$el.querySelector('[data-type="bar"]')
         this.$handle = this.$el.querySelector('[data-type="handle"]')
+        this.$header = this.$el.querySelector('[data-type="header"]')
 
         this.dragHandler = this.dragHandler.bind(this)
 
@@ -42,20 +44,22 @@ export class Slider {
         const parentPosition = getPosition(this.$el)
         let xPosition = event.clientX - parentPosition.x
             if (xPosition <= 0){
-                this.$handle.style.right = `${100}%`
-
+                this.$handle.style.left = `${0}%`
+                this.$header.style.width = `${0}%`
             }
             else if (xPosition >= this.$bar.offsetWidth){
-                this.$handle.style.right = `${0}%`
+                this.$handle.style.left = `${100}%`
+                this.$header.style.width = `${100}%`
             }
             else {
-                this.$handle.style.right = `${(this.$bar.offsetWidth - xPosition) * 100 / this.$bar.offsetWidth}%`
+                this.$handle.style.left = `${100 - (this.$bar.offsetWidth - xPosition) * 100 / this.$bar.offsetWidth}%`
+                this.$header.style.width = this.$handle.style.left
             }
 
             this.calc()
     }
     calc(){
-        const position = this.$handle.style.right.replace("%", '')
+        const position = this.$handle.style.left.replace("%", '')
 
         const step = this.step
         const min = this.min
