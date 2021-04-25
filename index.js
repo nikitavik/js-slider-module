2bff3fdb29d3db4e73205ef46af8325bdd8e42a9
Nikita Kornilov <nikitakornilov1998@gmail.com>
2021-04-25 07:20:18 +0300

    Add new options

--- index.js
+++ index.js
@@ -5,8 +5,22 @@ const slider = new Slider("#slider", {
         min: 10,
         max: 500,
         step: 10,
+        progressBar: true,
+        handleText: true,
+
 
     }
 )
 
 window.s = slider
+
+const slider2 = new Slider("#slider2", {
+        min: 10,
+        max: 500,
+        step: 10,
+        progressBar: true,
+        handleText: false,
+
+    }
+)
+window.s2 = slider2
