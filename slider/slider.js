050dd2f854bda4239b059eb2b17dc2d71ed42a1d
Nikita Kornilov <nikitakornilov1998@gmail.com>
2021-04-25 07:23:18 +0300

    Add value setter and start to code text option

--- slider/slider.js
+++ slider/slider.js
@@ -1,27 +1,28 @@
-const getTemplate = () => {
-    return `
-    <div class="slider-bar" data-type="bar" data->
-        <div class="slider-header" data-type="header"></div>
-        <span class="slider-handle" data-type="handle"></span>
-    </div>
-    `
-}
-
 export class Slider {
     constructor(selector, options) {
         this.$el = document.querySelector(selector)
+
+        this.options = options
 
         this.step = options.step
         this.max = options.max
         this.min = options.min
 
+
         this.#render()
         this.#setup()
+        this.#calc()
     }
+
     #render() {
         this.$el.classList.add("slider")
-        this.$el.innerHTML = getTemplate()
+        const {progressBar, handleText,} = this.options
+        this.$el.innerHTML = this.getTemplate({
+            haveProgressBar: progressBar,
+            haveHandleText: handleText,
+        })
     }
+
     #setup() {
         this.$bar = this.$el.querySelector('[data-type="bar"]')
         this.$handle = this.$el.querySelector('[data-type="handle"]')
@@ -29,54 +30,92 @@ export class Slider {
 
         this.dragHandler = this.dragHandler.bind(this)
 
-        this.$handle.style.right = `${100}%` // default position
-
         this.$bar.addEventListener("mousedown", ()=>{
             window.onmousemove = this.dragHandler
             window.onmouseup = ()=> {window.onmousemove = null}
         }, false)
+
         // this.$bar.addEventListener("mouseup", ()=>{
         //     window.onmousemove = null
         // }, false)
 
     }
+
     dragHandler(event){
         const parentPosition = getPosition(this.$el)
-        let xPosition = event.clientX - parentPosition.x
+        const xPosition = event.clientX - parentPosition.x
             if (xPosition <= 0){
-                this.$handle.style.left = `${0}%`
-                this.$header.style.width = `${0}%`
+                this.$handle.style.left = "0%"
+                this.$header.style.width = "0%"
             }
             else if (xPosition >= this.$bar.offsetWidth){
-                this.$handle.style.left = `${100}%`
-                this.$header.style.width = `${100}%`
+                this.$handle.style.left = "100%"
+                this.$header.style.width = "100%"
             }
             else {
                 this.$handle.style.left = `${100 - (this.$bar.offsetWidth - xPosition) * 100 / this.$bar.offsetWidth}%`
                 this.$header.style.width = this.$handle.style.left
             }
-
-            this.calc()
+        this.#calc()
     }
-    calc(){
-        const position = this.$handle.style.left.replace("%", '')
 
+    #handleTextHandler(){
+        if (this.options.handleText){
+            this.$header = this.$el.querySelector('[data-type="handle-text"]')
+            this.$header.textContent = this.getCurrent
+        }
+
+    }
+
+    #calc(){
+        const position = this.$handle.style.left.replace("%", '')
         const step = this.step
         const min = this.min
         const max = this.max
+        const value = Math.round((max - min) * (+position / 100)) + min
 
-        let value = Math.round((max - min) * (+position / 100)) + min
+        this.dragHandler = this.dragHandler.bind(this)
 
-        let result = Math.round(value / step) * step
+        this.current = Math.round(value / step) * step
 
-        document.getElementById("value").textContent = result
+        return Math.round(value / step) * step
     }
 
+
+    get getCurrent(){
+        return this.current
+    }
+
+    set setCurrent(current) {
+        if (current >= this.min && current <= this.max) {
+            const fraction = current * 100 / this.max
+            this.$handle.style.left = `${fraction}%`
+            this.$header.style.width = this.$handle.style.left
+            console.log(fraction)
+            this.current = current
+        }
+        else {
+            console.error("Incorrect Value")
+        }
+    }
+
+    getTemplate(templateOptions) {
+        return `
+            <div class="slider-bar" data-type="bar" data->
+                <div class="slider-header" data-type="header"></div>
+                <div class="slider-handle" data-type="handle">
+                    ${templateOptions.haveHandleText ? 
+                        `<div class="slider-handle-text" data-type="handle-text">${this.min}</div>`
+                        : ""}
+                </div>
+            </div>
+        `
+    }
 }
 
 function getPosition(el) {
-    var xPosition = 0;
-    var yPosition = 0;
+    let xPosition = 0;
+    let yPosition = 0;
 
     while (el) {
         xPosition += (el.offsetLeft - el.scrollLeft + el.clientLeft);
